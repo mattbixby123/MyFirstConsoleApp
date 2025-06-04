a0ca67f11fe4c60b117a68d8c92b6d044a388253
mattbixby123 <matthew.bixby1@gmail.com>
2025-06-04 17:00:31 -0400

    Add project files.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Runtime.InteropServices;
+
+OperatorExamples();
+
+void OperatorExamples()
+{
+    // this statement declares a variable and sets it to 3
+    int width = 3;
+
+    // the ++ operator increments a variable  (adds 1 to it)
+    width++;
+
+    // Delcare two more int variables to hold numbers and
+    // use the + and * operators to add and multiply values
+    int height = 2 + 4;
+    int area = width * height;
+    Console.WriteLine(area);
+
+    // the next two statements declare starting variables
+    string result = "The area";
+    result = result + " is " + area;
+    Console.WriteLine(result);
+
+    // a boolean variable is either true or false
+    bool truthValue = true;
+    Console.WriteLine(truthValue);
+}
